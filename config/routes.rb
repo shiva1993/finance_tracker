830279753b44f18abc87a34c62eed9cfcc7626da
shivaraj <shiva1993>
2016-01-11 19:58:28 +0530

    Add stocks and ajax for ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
- resources :welcome
+  devise_for :users
+  
+resources :welcome 
   root 'welcome#index'
+ 
+ get 'my_portfolio',to: "users#my_portfolio"
+ get 'search_stocks',to: 'stocks#search'
 end
